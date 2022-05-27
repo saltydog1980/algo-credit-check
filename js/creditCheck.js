cd17c15628b04644abf664f3cb46a5f9c202e3da
saltydog1980 <mikeelambert99@gmail.com>
2022-05-27 09:43:53 -0400

    commented out the code

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,12 +1,20 @@
 exports.creditCheck = function(str) {
+    //splitting the input string number
     let baseArr = str.split('')
+    //looping over the array by 2
     for (let i = 0; i < baseArr.length; i = i + 2) {
+        //if num * 2 is over 9 (over single digit) I am creating variable num by doubling the num
         if (baseArr[i]*2 > 9){
             num = baseArr[i]*2
+            //I am re-assigning the value at index i to the double digit num spilt (takes to string) running map to 
+            //convert back to int and then reducing the values to a single value that gets assined to the index in basearr
             baseArr[i] = num.toString().split('').map(Number).reduce((a,b) => a+b)
         } else {
+            //so all single digits I am just doubling and reassigning to their original index
             baseArr[i] = baseArr[i]*2
         }
     }
+    //return statement. Take baseArr to map to convert indexes we did not double into ints then reducing the entire array
+    //if the reduce % 10 is zer then it returns valid else invalid
     return baseArr.map(Number).reduce((a, b) => a + b) % 10 === 0 ? 'The number is valid!' : 'The number is invalid!'
 }
--- python/credit_check.py
+++ python/credit_check.py
@@ -1,12 +1,21 @@
 import functools
 def credit_check(string):
+    #creating base list by taking map over the string converting to in and casting to list
     base_list = list(map(int,string))
+    #for looping over the base list by 2
     for x in range(0, len(base_list), 2):
+        #if num *2 over single digit
         if base_list[x]*2 > 9:
+            #creating variable nums by taking the num *2 to string so I can map/list it to break into a list with ints being each digit 
             nums = list(map(int, str(base_list[x]*2)))
+            #creating variable fin by taking the nums list and reducing it
             fin = functools.reduce(lambda agg, item : agg + item, nums)
+            #re-assigning fin to the original nums index in the base list
             base_list[x] = fin
         else:
+            #if single digit just doubling and re-assigning to the same index in base list
             base_list[x] = base_list[x]*2
+    #Rapheal's favorite. Had to do a ternary assigning the out put to variable a so that if the reduce of the list % 10 was 0 a is valid else invalid
     a = 'The number is valid!' if functools.reduce(lambda agg, item : agg + item, base_list) % 10 == 0 else 'The number is invalid!'
+    #returning variable a
     return a
