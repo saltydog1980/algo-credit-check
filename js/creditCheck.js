a1845d6f4ab3f9878e161cf1524dcaf981313365
saltydog1980 <mikeelambert99@gmail.com>
2022-05-26 21:07:54 -0400

    final code

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,12 @@
 exports.creditCheck = function(str) {
+    let baseArr = str.split('')
+    for (let i = 0; i < baseArr.length; i = i + 2) {
+        if (baseArr[i]*2 > 9){
+            num = baseArr[i]*2
+            baseArr[i] = num.toString().split('').map(Number).reduce((a,b) => a+b)
+        } else {
+            baseArr[i] = baseArr[i]*2
+        }
+    }
+    return baseArr.map(Number).reduce((a, b) => a + b) % 10 === 0 ? 'The number is valid!' : 'The number is invalid!'
 }
--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,12 @@
-def credit_check(str):
-    pass
-
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
-
+import functools
+def credit_check(string):
+    base_list = list(map(int,string))
+    for x in range(0, len(base_list), 2):
+        if base_list[x]*2 > 9:
+            nums = list(map(int, str(base_list[x]*2)))
+            fin = functools.reduce(lambda agg, item : agg + item, nums)
+            base_list[x] = fin
+        else:
+            base_list[x] = base_list[x]*2
+    a = 'The number is valid!' if functools.reduce(lambda agg, item : agg + item, base_list) % 10 == 0 else 'The number is invalid!'
+    return a
